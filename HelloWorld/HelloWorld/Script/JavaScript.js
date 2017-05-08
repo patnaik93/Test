f0362fb93f9a3e3eb3e12eeae8b07496c6b4a4c2
patnaik93 <patnaik93@gmail.com>
2017-05-08 19:34:02 +0530

    Hello

---
+++ HelloWorld/HelloWorld/Script/JavaScript.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.min.js" />
+
+var myApp = angular.module("ModuleName", []); //module and its dependencies
+
+var control = function ($scope) {
+    var employee = {
+        firstName: "Charith",
+        lastName: "Patnaik",
+        age: "23",
+        pic: "C:\Users\ppatnaik\Source\Repos\Test\HelloWorld\HelloWorld\Images\Charith.png"
+    };
+    $scope.emp = employee;
+    $scope.message = "Employee Details";
+};
+
+myApp.controller("ControllerName", control); //myApp is module and control is controller function 
